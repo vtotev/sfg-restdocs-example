0526b323a9da7c9055fa0e89f45682c7dcc780d7
Valeri Totev <valeri.totev@blankfactor.com>
2023-10-18 15:05:57 +0300

    SFG Spring REST Docs Example project

---
+++ src/main/java/guru/springframework/sfgrestdocsexample/repositories/BeerRepository.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrestdocsexample.repositories;
+
+import guru.springframework.sfgrestdocsexample.domain.Beer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-17.
+ */
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>, CrudRepository<Beer, UUID> {
+}
---
+++ src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
@@ -0,0 +1,54 @@
+package guru.springframework.sfgrestdocsexample.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-12.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    @Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
+    private OffsetDateTime createdDate;
+
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    private String beerName;
+
+    @NotNull
+    private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
+    private Long upc;
+
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
